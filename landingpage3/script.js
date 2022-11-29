a60d4967908bf0ac5318a785aad448c5eaeb1521
Danielae11 <114977009+Danielae11@users.noreply.github.com>
2022-11-29 15:06:13 +0700

    Add files via upload

--- landingpage3/script.js
+++ landingpage3/script.js
@@ -1,34 +1,128 @@
 //CARAOUSEL DOCTOR STRANGE
+function carouselAction() {
+    const dotAction = document.querySelectorAll(".dot-action");
+    const sliderAction = document.querySelectorAll(".slider-action");
 
-const dot = document.querySelectorAll(".dot");
-const containerSlide = document.querySelectorAll(".container-slide");
+    let current = 0;
 
-let current = 0;
-
-dot.forEach((slide, index) => {
-    slide.addEventListener("click", function() {
-        changeDots(this);
-        nextSlide(index);
+    dotAction.forEach((slide, index) => {
+        slide.addEventListener("click", function() {
+            changeDots(this);
+            nextSlide(index);
+        });
     });
-});
 
-function changeDots(dots) {
-    dot.forEach(slide => {
-        slide.classList.remove("active");
+    function changeDots(dots) {
+        dotAction.forEach(slide => {
+            slide.classList.remove("active");
+        });
+        dots.classList.add("active");
+    }
+
+    function nextSlide(pageNumber) {
+        const nextPage = sliderAction[pageNumber];
+        const currentPage = sliderAction[current];
+
+        nextPage.style.transform = "translateX(0%)";
+        currentPage.style.transform = "translateX(-100%)";
+
+        current = pageNumber;
+    }
+}
+carouselAction();
+function carouselAdventure() {
+    const dotAdventure = document.querySelectorAll(".dot-adventure");
+    const sliderAdventure = document.querySelectorAll(".slider-adventure");
+
+    let current = 0;
+
+    dotAdventure.forEach((slide, index) => {
+        slide.addEventListener("click", function() {
+            changeDots(this);
+            nextSlide(index);
+        });
     });
-    dots.classList.add("active");
+
+    function changeDots(dots) {
+        dotAdventure.forEach(slide => {
+            slide.classList.remove("active");
+        });
+        dots.classList.add("active");
+    }
+
+    function nextSlide(pageNumber) {
+        const nextPage = sliderAdventure[pageNumber];
+        const currentPage = sliderAdventure[current];
+
+        nextPage.style.transform = "translateX(0%)";
+        currentPage.style.transform = "translateX(-100%)";
+
+        current = pageNumber;
+    }
 }
+carouselAdventure();
+function carouselComedy() {
+    const dotComedy = document.querySelectorAll(".dot-comedy");
+    const sliderComedy = document.querySelectorAll(".slider-comedy");
 
-function nextSlide(pageNumber) {
-    const nextPage = containerSlide[pageNumber];
-    const currentPage = containerSlide[current];
+    let current = 0;
 
-    nextPage.style.transform = "translateX(0%)";
-    currentPage.style.transform = "translateX(-100%)";
+    dotComedy.forEach((slide, index) => {
+        slide.addEventListener("click", function() {
+            changeDots(this);
+            nextSlide(index);
+        });
+    });
 
-    current = pageNumber;
+    function changeDots(dots) {
+        dotComedy.forEach(slide => {
+            slide.classList.remove("active");
+        });
+        dots.classList.add("active");
+    }
+
+    function nextSlide(pageNumber) {
+        const nextPage = sliderComedy[pageNumber];
+        const currentPage = sliderComedy[current];
+
+        nextPage.style.transform = "translateX(0%)";
+        currentPage.style.transform = "translateX(-100%)";
+
+        current = pageNumber;
+    }
 }
+carouselComedy();
+function carouselHorror() {
+    const dotHorror = document.querySelectorAll(".dot-horror");
+    const sliderHorror = document.querySelectorAll(".slider-horror");
 
+    let current = 0;
+
+    dotHorror.forEach((slide, index) => {
+        slide.addEventListener("click", function() {
+            changeDots(this);
+            nextSlide(index);
+        });
+    });
+
+    function changeDots(dots) {
+        dotHorror.forEach(slide => {
+            slide.classList.remove("active");
+        });
+        dots.classList.add("active");
+    }
+
+    function nextSlide(pageNumber) {
+        const nextPage = sliderHorror[pageNumber];
+        const currentPage = sliderHorror[current];
+
+        nextPage.style.transform = "translateX(0%)";
+        currentPage.style.transform = "translateX(-100%)";
+
+        current = pageNumber;
+    }
+}
+carouselHorror();
 //CARAOUSEL RECOMENDED
 
 var slideIndex = 1;
@@ -40,21 +134,70 @@ function onSlide(n) {
 
 function showSlide(n) {
     var i;
-    var slides = document.getElementsByClassName("recomended-slider");
-
-    if (n > slides.length) {
+    var slidesAction = document.getElementsByClassName("recomended-slider-action");
+    var slidesAdventure = document.getElementsByClassName("recomended-slider-adventure");
+    var slidesComedy = document.getElementsByClassName("recomended-slider-comedy");
+    var slidesHorror = document.getElementsByClassName("recomended-slider-horror");
+//ACTION    
+    if (n > slidesAction.length) {
         slideIndex = 1;
     }
 
     if (n < 1) {
-        slideIndex = slides.length;
+        slideIndex = slidesAction.length;
     }
 
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    for (i = 0; i < slidesAction.length; i++) {
+        slidesAction[i].style.display = "none";
     }
 
-    slides[slideIndex - 1].style.display = "block";
+    slidesAction[slideIndex - 1].style.display = "block";
+
+//ADVENTURE
+    if (n > slidesAdventure.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slidesAdventure.length;
+    }
+
+    for (i = 0; i < slidesAdventure.length; i++) {
+        slidesAdventure[i].style.display = "none";
+    }
+
+    slidesAdventure[slideIndex - 1].style.display = "block";
+
+//COMEDY
+    if (n > slidesComedy.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slidesComedy.length;
+    }
+
+    for (i = 0; i < slidesComedy.length; i++) {
+        slidesComedy[i].style.display = "none";
+    }
+
+    slidesComedy[slideIndex - 1].style.display = "block";
+
+//HORROR
+    if (n > slidesHorror.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slidesHorror.length;
+    }
+
+    for (i = 0; i < slidesHorror.length; i++) {
+        slidesHorror[i].style.display = "none";
+    }
+
+    slidesHorror[slideIndex - 1].style.display = "block";
+
 }
 
 //INDICATOR COLOR NAV-CONTENT MENU
@@ -77,22 +220,41 @@ function navContentMenu() {
 }
 navContentMenu();
 
-//INDICATOR COLOR NAV GENRE
+//INDICATOR COLOR NAV GENRE 
 
 function NavGenre() {
-    const genre = document.querySelectorAll(".genre div");
+    const buttonGenre = document.querySelectorAll(".genre div");
+    const containerMain = document.querySelectorAll(".container-main");
 
-    genre.forEach((color, index) => {
+    let first = 0;
+    buttonGenre.forEach((color, index) => {
         color.addEventListener("click", function() {
             getColor(this);
+            changeGenre(index);
         });
     });
 
     function getColor(get) {
-        genre.forEach(color => {
+        buttonGenre.forEach(color => {
             color.classList.remove("indicator");
         })
         get.classList.add("indicator");
+    }
+    function changeGenre(genre) {
+        const nextGenre = containerMain[genre];
+        const currentGenre = containerMain[first];
+        const nextClone = document.querySelectorAll(".container-clone")[genre];
+        const currentClone = document.querySelectorAll(".container-clone")[first];
+
+        nextGenre.style.opacity = "1";
+        nextGenre.style.pointerEvents = "all";
+        currentGenre.style.opacity = "0";
+        currentGenre.style.pointerEvents = "none";
+        nextClone.style.opacity = "1";
+        nextClone.style.pointerEvents = "all";
+        currentClone.style.opacity = "0";
+        currentClone.style.pointerEvents = "none";
+        first = genre;
     }
 }
 NavGenre();
